e23a547c4d704d89255005c78c15f79540c8acfe
alozanski <alozanski@vp.pl>
2022-04-26 18:27:33 +0200

    Initial commit

---
+++ src/main/java/Account.java
@@ -0,0 +1,26 @@
+public class Account {
+
+    private String name;
+    private String surname;
+    private String accNumber;
+    private long balance;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+   public String getSurname(){
+        return surname;
+   }
+   public void setSurname( String surname){
+        this.surname = surname;
+   }
+   public String getAccNumber(){
+        return accNumber;
+   }
+   public void setAccNumber(String accNumber){
+        this.accNumber= accNumber;
+   }
+}
